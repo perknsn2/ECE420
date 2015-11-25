e8ab99156aff4d0888edd20b65bc18aea7660b55
Caleb <perknsn2@illinois.edu>
2015-11-25 13:43:39 -0600

    Adds OpenCV Library and better launching

---
+++ BuildingDetector/app/src/main/java/com/example/caleb/buildingdetector/CameraSupport.java
@@ -0,0 +1,7 @@
+package com.example.caleb.buildingdetector;
+
+/**
+ * Created by Caleb on 11/25/2015.
+ */
+public class CameraSupport {
+}
--- BuildingDetector/app/src/main/java/com/example/caleb/buildingdetector/MainActivity.java
+++ BuildingDetector/app/src/main/java/com/example/caleb/buildingdetector/MainActivity.java
@@ -1,18 +1,69 @@
 package com.example.caleb.buildingdetector;
 
+import android.app.Activity;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+    private static final String TAG = "MainActivity";
+
+    private MenuItem mItemStream;
+    private MenuItem mItemSnapshot;
+
+    private BaseLoaderCallback mOpenCVCallBack = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status){
+                case LoaderCallbackInterface.SUCCESS:
+                {
+                    Log.d(TAG, "OpenCV loaded Successfully");
+
+                    //Load Library
+                    System.loadLibrary("opencv_java3");
+                } break;
+
+                case LoaderCallbackInterface.MARKET_ERROR:
+                {
+                    Log.d(TAG, "Google Play service is not accessible");
+                    AlertDialog MarketErrorMessage = new AlertDialog.Builder(mAppContext).create();
+                    MarketErrorMessage.setTitle("OpenCV Manager");
+                    MarketErrorMessage.setMessage("Google Play service is not accessible!");
+                    MarketErrorMessage.setCancelable(false);
+                    MarketErrorMessage.setButton(AlertDialog.BUTTON_POSITIVE, "OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ((Activity) mAppContext).finish();
+                        }
+                    });
+                    MarketErrorMessage.show();
+                } break;
+
+                default:
+                {
+                    super.onManagerConnected(status);
+                } break;
+            }
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -26,10 +77,29 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        // Creating Camera intent
+//        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//        startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d(TAG, "onResume");
+        super.onResume();
+
+        Log.d(TAG, "Load OpenCV Library");
+        if(!OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0,this,mOpenCVCallBack)){
+            Log.e(TAG, "Failed to load OpenCV library");
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        Log.d(TAG, "onCreateOptionsMenu");
+
+        mItemStream = menu.add("Stream");
+        mItemSnapshot = menu.add("Snapshot");
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
@@ -37,6 +107,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        Log.d(TAG, "onOptionsItemSelected");
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
@@ -45,6 +116,12 @@ public class MainActivity extends AppCompatActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
+        }
+        else if (item == mItemSnapshot){
+            Log.d(TAG, "Snapshot");
+        }
+        else if (item == mItemStream){
+            Log.d(TAG, "Stream");
         }
 
         return super.onOptionsItemSelected(item);
