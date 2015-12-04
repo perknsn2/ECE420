2e0ab239ca7faf28690b8b58471579f8454aa00e
Caleb <perknsn2@illinois.edu>
2015-12-04 16:02:15 -0600

    Completes basic functionality of the app - image recognition - minor UI changes

--- BuildingDetector/app/src/main/java/com/example/caleb/buildingdetector/MainActivity.java
+++ BuildingDetector/app/src/main/java/com/example/caleb/buildingdetector/MainActivity.java
@@ -1,31 +1,20 @@
 package com.example.caleb.buildingdetector;
 
-import android.app.Activity;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import org.opencv.android.BaseLoaderCallback;
-import org.opencv.android.LoaderCallbackInterface;
-import org.opencv.android.OpenCVLoader;
 import org.opencv.android.Utils;
-import org.opencv.core.DMatch;
-import org.opencv.core.KeyPoint;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfByte;
 import org.opencv.core.MatOfDMatch;
@@ -37,82 +26,129 @@ import org.opencv.features2d.FeatureDetector;
 import org.opencv.features2d.Features2d;
 import org.opencv.imgproc.Imgproc;
 
-import com.example.caleb.buildingdetector.CameraSupport;
-
-import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
-
-import static com.example.caleb.buildingdetector.R.layout.content_main;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
     private static final int CAPTURE_VIDEO_ACTIVITY_REQUEST_CODE = 200;
     private static final String TAG = "MainActivity";
-    private Uri fileUri;
 
+    static {
+        System.loadLibrary("opencv_java3");
+    }
+
+    private Uri fileUri;
     private MenuItem mItemStream;
     private MenuItem mItemSnapshot;
-
     private Bitmap mBitmap;
     private Mat mMat;
     private Features2d mFeatures2d;
     private FeatureDetector mFeatureDetector;
     private MatOfKeyPoint mMatofKeyPoint;
-
+    private DescriptorMatcher mDescriptorMatcher;
+    private Mat dbImgMat;
+    private List<Mat> mDescriptorList;
     private ImageView mImageView;
+    private TextView mTextView;
 
-    private BaseLoaderCallback mOpenCVCallBack = new BaseLoaderCallback(this) {
-        @Override
-        public void onManagerConnected(int status) {
-            switch (status){
-                case LoaderCallbackInterface.SUCCESS:
-                {
-                    Log.d(TAG, "OpenCV loaded Successfully");
+//    private BaseLoaderCallback mOpenCVCallBack = new BaseLoaderCallback(this) {
+//        @Override
+//        public void onManagerConnected(int status) {
+//            switch (status){
+//                case LoaderCallbackInterface.SUCCESS:
+//                {
+//                    Log.d(TAG, "OpenCV loaded Successfully");
+//
+//                    dbImgMat = new Mat();
+//                } break;
+//
+//                case LoaderCallbackInterface.MARKET_ERROR:
+//                {
+//                    Log.d(TAG, "Google Play service is not accessible");
+//                    AlertDialog MarketErrorMessage = new AlertDialog.Builder(mAppContext).create();
+//                    MarketErrorMessage.setTitle("OpenCV Manager");
+//                    MarketErrorMessage.setMessage("Google Play service is not accessible!");
+//                    MarketErrorMessage.setCancelable(false);
+//                    MarketErrorMessage.setButton(AlertDialog.BUTTON_POSITIVE, "OK", new DialogInterface.OnClickListener() {
+//                        @Override
+//                        public void onClick(DialogInterface dialog, int which) {
+//                            ((Activity) mAppContext).finish();
+//                        }
+//                    });
+//                    MarketErrorMessage.show();
+//                } break;
+//
+//                default:
+//                {
+//                    super.onManagerConnected(status);
+//                } break;
+//            }
+//        }
+//    };
 
-                    // Creating Camera intent
-                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
-                    fileUri = CameraSupport.getOutputMediaFileUri(CameraSupport.MEDIA_TYPE_IMAGE);
-                    intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
-
-                    startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
-                } break;
-
-                case LoaderCallbackInterface.MARKET_ERROR:
-                {
-                    Log.d(TAG, "Google Play service is not accessible");
-                    AlertDialog MarketErrorMessage = new AlertDialog.Builder(mAppContext).create();
-                    MarketErrorMessage.setTitle("OpenCV Manager");
-                    MarketErrorMessage.setMessage("Google Play service is not accessible!");
-                    MarketErrorMessage.setCancelable(false);
-                    MarketErrorMessage.setButton(AlertDialog.BUTTON_POSITIVE, "OK", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            ((Activity) mAppContext).finish();
-                        }
-                    });
-                    MarketErrorMessage.show();
-                } break;
-
-                default:
-                {
-                    super.onManagerConnected(status);
-                } break;
-            }
-        }
-    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.d(TAG, "onCreate");
 
         super.onCreate(savedInstanceState);
 
-        Log.d(TAG, "Load OpenCV Library");
-        if (!OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_8, this, mOpenCVCallBack)) {
-            Log.e(TAG, "Failed to load OpenCV library");
+        /** So using the async loader doesn't load the library in time so I switched to static
+         * loading.  Not sure which is better... wait. If open cv isn't installed this may cause a
+         * problem. Not worth checking out now though.
+         */
+//        Log.d(TAG, "Load OpenCV Library");
+//        if (!OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_8, this, mOpenCVCallBack)) {
+//            Log.e(TAG, "Failed to load OpenCV library");
+//        }
+
+        Bitmap dbImgBitmap;
+        mDescriptorList = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            if (i == 0)
+                dbImgBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.oscilloscope);
+            else if (i == 1)
+                dbImgBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.multimeter);
+            else if (i == 2)
+                dbImgBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.function_gen);
+            else if (i == 3)
+                dbImgBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.keyboard);
+            else if (i == 4)
+                dbImgBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.mouse);
+            else
+                dbImgBitmap = null;
+
+            dbImgMat = new Mat();
+
+            Utils.bitmapToMat(dbImgBitmap, dbImgMat);
+
+            Mat dbBwMat = new Mat();
+            org.opencv.imgproc.Imgproc.cvtColor(dbImgMat, dbBwMat, Imgproc.COLOR_BGR2GRAY);
+
+            mFeatureDetector = FeatureDetector.create(FeatureDetector.ORB);
+            MatOfKeyPoint dbKeypoints = new MatOfKeyPoint();
+            mFeatureDetector.detect(dbBwMat, dbKeypoints);
+
+            Mat dbImgDesc = new Mat();
+
+            DescriptorExtractor siftExtractor = DescriptorExtractor.create(DescriptorExtractor.ORB);
+            siftExtractor.compute(dbBwMat, dbKeypoints, dbImgDesc);
+            mDescriptorList.add(dbImgDesc);
         }
+
+        mDescriptorMatcher = DescriptorMatcher.create(DescriptorMatcher.BRUTEFORCE_L1);
+
+//        mDescriptorMatcher.add(mDescriptorList);
+
+        /** Creating Camera intent **/
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        fileUri = CameraSupport.getOutputMediaFileUri(CameraSupport.MEDIA_TYPE_IMAGE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+
+        startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
 
 //        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
@@ -170,85 +206,122 @@ public class MainActivity extends AppCompatActivity {
                 setContentView(R.layout.content_main);
 
                 mImageView = (ImageView) findViewById(R.id.imageView);
+                mTextView = (TextView) findViewById(R.id.imageDescription);
 
                 try {
                     // Get image bitmap
                     BitmapFactory.Options options = new BitmapFactory.Options();
-//                    options.outHeight = mImageView.getHeight();
-//                    options.outWidth = mImageView.getWidth();
 
                     //Bitmap is type ARGB_8888
                     Bitmap mBitmap = BitmapFactory.decodeStream(this.getContentResolver().openInputStream(fileUri), null, options);
-                    Bitmap dbImgBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.oscilloscope);
 
                     mMat = new Mat();
-                    Mat dbImgMat = new Mat();
 
                     Utils.bitmapToMat(mBitmap, mMat);
-                    Utils.bitmapToMat(dbImgBitmap, dbImgMat);
 
                     Mat newMat = new Mat();
                     org.opencv.imgproc.Imgproc.cvtColor(mMat, newMat, Imgproc.COLOR_BGR2GRAY);
-                    Mat dbBwMat = new Mat();
-                    org.opencv.imgproc.Imgproc.cvtColor(dbImgMat, dbBwMat, Imgproc.COLOR_BGR2GRAY);
 
                     mFeatureDetector = FeatureDetector.create(FeatureDetector.ORB);
 
                     mMatofKeyPoint = new MatOfKeyPoint();
-                    MatOfKeyPoint dbKeypoints = new MatOfKeyPoint();
 
                     mFeatureDetector.detect(newMat, mMatofKeyPoint);
-                    mFeatureDetector.detect(dbBwMat, dbKeypoints);
 
                     Mat imgDesc = new Mat();
-                    Mat dbImgDesc = new Mat();
 
                     DescriptorExtractor siftExtractor = DescriptorExtractor.create(DescriptorExtractor.ORB);
                     siftExtractor.compute(newMat, mMatofKeyPoint, imgDesc);
-                    siftExtractor.compute(dbBwMat, dbKeypoints, dbImgDesc);
 
-                    DescriptorMatcher siftMatcher = DescriptorMatcher.create(DescriptorMatcher.BRUTEFORCE_L1);
-
-                    MatOfDMatch imgMatches = new MatOfDMatch();
-                    siftMatcher.match(imgDesc, dbImgDesc, imgMatches);
-
-                    List<DMatch> matchList = imgMatches.toList();
-                    float average = 0;
-                    float min = matchList.get(0).distance;
-                    float max = min;
-                    for(int i = 0; i<matchList.size(); i++)
-                    {
-                        float dist = matchList.get(i).distance;
-                        if(dist<min)
-                            min = dist;
-                        else if(dist>max)
-                            max = dist;
-                        average += dist;
+                    int bestMatchIdx = -1;
+                    MatOfDMatch bestMatch = new MatOfDMatch();
+                    float avgMinSizeofMatches = Float.MAX_VALUE;
+                    for (int i = 0; i < mDescriptorList.size(); i++) {
+                        float avgSizeOfMatches = 0;
+                        MatOfDMatch curMatch = new MatOfDMatch();
+                        mDescriptorMatcher.match(imgDesc, mDescriptorList.get(i), curMatch);
+                        avgSizeOfMatches = matchHelp.average(curMatch);
+                        if (avgSizeOfMatches <= avgMinSizeofMatches) {
+                            avgMinSizeofMatches = avgSizeOfMatches;
+                            bestMatchIdx = i;
+                            bestMatch = curMatch;           //holds matches of best matched image
+                        }
                     }
-                    average /= matchList.size();
-                    int numGoodMatch = 0;
-                    for(int i = 0; i<matchList.size(); i++)
-                    {
-                        if(matchList.get(i).distance < (min + 0.85*(average-min)))
-                            numGoodMatch++;
-                    }
-                    List<KeyPoint> dbImgKeyList = dbKeypoints.toList();
-                    List<KeyPoint> imgKeyList = mMatofKeyPoint.toList();
 
-                    double numDbKeys = (double)dbImgKeyList.size();
-                    double numImgKeys = (double)imgKeyList.size();
+                    if (avgMinSizeofMatches > 1300)
+                    {
+                        bestMatchIdx = -1;
+                        Toast.makeText(this, "No match found", Toast.LENGTH_LONG).show();
+                        return;
+                    }
+
+//                    List<DMatch> matchList = imgMatches.toList();
+//                    float average = 0;
+//                    float min = matchList.get(0).distance;
+//                    float max = min;
+//                    for(int i = 0; i<matchList.size(); i++)
+//                    {
+//                        float dist = matchList.get(i).distance;
+//                        if(dist<min)
+//                            min = dist;
+//                        else if(dist>max)
+//                            max = dist;
+//                        average += dist;
+//                    }
+//                    average /= matchList.size();
+//                    int numGoodMatch = 0;
+//                    for(int i = 0; i<matchList.size(); i++)
+//                    {
+//                        if(matchList.get(i).distance < (min + 0.85*(average-min)))
+//                            numGoodMatch++;
+//                    }
+//                    List<KeyPoint> dbImgKeyList = dbKeypoints.toList();
+//                    List<KeyPoint> imgKeyList = mMatofKeyPoint.toList();
+//
+//                    double numDbKeys = (double)dbImgKeyList.size();
+//                    double numImgKeys = (double)imgKeyList.size();
+//
+                    Mat imgMat = new Mat();
+                    Bitmap dbImgBitmap = null;
+                    String imageDescription = "The image is a ";
+
+                    if (0 == bestMatchIdx) {
+                        dbImgBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.oscilloscope);
+                        imageDescription += "oscilloscope";
+                    } else if (1 == bestMatchIdx) {
+                        dbImgBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.multimeter);
+                        imageDescription += "multimeter";
+                    } else if (2 == bestMatchIdx) {
+                        dbImgBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.function_gen);
+                        imageDescription += "function generator";
+                    } else if (3 == bestMatchIdx) {
+                        dbImgBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.keyboard);
+                        imageDescription += "keyboard";
+                    } else if (4 == bestMatchIdx) {
+                        dbImgBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.mouse);
+                        imageDescription += "mouse";
+                    }
+
+                    mTextView.setText(imageDescription);
+                    Utils.bitmapToMat(dbImgBitmap, imgMat);
+
+                    Mat grayImgMat = new Mat();
+                    org.opencv.imgproc.Imgproc.cvtColor(imgMat, grayImgMat, Imgproc.COLOR_BGR2GRAY);
+
+                    MatOfKeyPoint dbKeypoints = new MatOfKeyPoint();
+                    mFeatureDetector.detect(grayImgMat, dbKeypoints);
 
                     Mat outImg = new Mat();
                     Scalar blue = new Scalar(0,0,255);
                     Scalar green = new Scalar(0,255,0);
                     MatOfByte matchMask = new MatOfByte();
-                    if(((double)numGoodMatch/numDbKeys > 0.35) || ((double)numGoodMatch/numImgKeys > 0.35))
-                        Features2d.drawMatches(newMat, mMatofKeyPoint, dbBwMat, dbKeypoints, imgMatches, outImg, blue, green, matchMask, 0);
-                    else
-                    {
-                        MatOfDMatch noMatch = new MatOfDMatch();
-                        Features2d.drawMatches(newMat, mMatofKeyPoint, dbBwMat, dbKeypoints, noMatch, outImg, blue, green, matchMask, 0);
-                    }
+//                    if(((double)numGoodMatch/numDbKeys > 0.35) || ((double)numGoodMatch/numImgKeys > 0.35))
+                    Features2d.drawMatches(newMat, mMatofKeyPoint, grayImgMat, dbKeypoints, bestMatch, outImg, blue, green, matchMask, 0);
+//                    else
+//                    {
+//                        MatOfDMatch noMatch = new MatOfDMatch();
+//                        Features2d.drawMatches(newMat, mMatofKeyPoint, dbBwMat, dbKeypoints, noMatch, outImg, blue, green, matchMask, 0);
+//                    }
 //                    Features2d.drawKeypoints(dbBwMat,dbKeypoints,outImg, blue, 0);
 
                     /**   Testing code **/
